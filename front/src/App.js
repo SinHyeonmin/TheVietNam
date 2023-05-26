25800a6efe722699797c38e67e810b06bea359bc
SinHyeonmin <tlsgusals125@naver.com>
2023-05-26 15:10:57 +0900

    사이드바 헤더 홈 메인페이지

--- front/src/App.js
+++ front/src/App.js
@@ -1,15 +1,8 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/home'
-import Mainpage from './pages/mainpage'
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home></Home>} />
-        <Route path="/mainpage" element={<Mainpage></Mainpage>} />
-      </Routes>
-    </Router>
+    <Home></Home>
   );
 }
 
--- front/src/components/topnav.js
+++ front/src/components/topnav.js
@@ -1,7 +1,6 @@
 import './topnav.css';
 import { BsInstagram } from 'react-icons/bs';
 import { AiOutlineClose } from 'react-icons/ai';
-import { Link } from 'react-router-dom';
 
 function Nav({ isNavOpen, handleNavToggle }) {
   return (
@@ -11,9 +10,7 @@ function Nav({ isNavOpen, handleNavToggle }) {
           {isNavOpen ? <AiOutlineClose size="36px" onClick={handleNavToggle} /> : null}
         </div>
         <ul>
-          <li>
-            <Link to="/mainpage">더빛남</Link>
-          </li>
+          <li>더빛남</li>
           <li>창업안내</li>
           <li>고객장소</li>
           <li className="nav_instagram">
